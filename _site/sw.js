feb246a4f340223cb8c67bd3774d765cc7f22d15
tjxj <1030999281@qq.com>
2023-01-08 23:15:01 +0800

    1.0

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230108.231420';
+const cacheName = 'chirpy-20230108.231456';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
