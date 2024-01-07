5823757857f413dc124661289b2e3a2364802b4d
Z Z <zz@Zs-Mac-mini.local>
2024-01-07 11:03:29 +0800

    1.0

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240102.132639';
+const cacheName = 'chirpy-20240107.110302';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
