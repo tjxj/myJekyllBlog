633589bacafa47e1bf2584586806fcd1bb3c5d46
tjxj <1030999281@qq.com>
2024-01-30 10:36:23 +0800

    1.0

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240113.012256';
+const cacheName = 'chirpy-20240130.103620';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
